b6f405649cbd2043f90a555c6067b29e50cbee2f
Adrian Pereira Guerra <adries.91@gmail.com>
2019-08-18 12:53:22 +0200

    Build board dinamically

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ class Board extends React.Component {
     renderSquare(i) {
         return (
             <Square
+                key={i}
                 value={this.props.squares[i]}
                 onClick={() => this.props.onClick(i)}
             />
@@ -21,25 +22,23 @@ class Board extends React.Component {
     }
 
     render() {
-        return (
-            <div>
-                <div className="board-row">
-                    {this.renderSquare(0)}
-                    {this.renderSquare(1)}
-                    {this.renderSquare(2)}
+        const width = Math.sqrt(this.props.squares.length);
+        let rows = [];
+        for (let i = 0; i < width; i++) {
+            let cols = [];
+            for (let j = 0; j < width; j++) {
+                const square = this.renderSquare(i * width + j);
+                cols.push(square);
+            }
+
+            rows.push(
+                <div key={i} className="board-row">
+                    {cols}
                 </div>
-                <div className="board-row">
-                    {this.renderSquare(3)}
-                    {this.renderSquare(4)}
-                    {this.renderSquare(5)}
-                </div>
-                <div className="board-row">
-                    {this.renderSquare(6)}
-                    {this.renderSquare(7)}
-                    {this.renderSquare(8)}
-                </div>
-            </div>
-        );
+            );
+        }
+
+        return <div>{rows}</div>;
     }
 }
 
