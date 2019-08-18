692fd9d2a35af08407d811108d0c5b794db023ec
Adrian Pereira Guerra <adries.91@gmail.com>
2019-08-18 12:57:31 +0200

    docs folder for github pages

---
+++ docs/precache-manifest.a81b927d4df9cebef336f6883568b962.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9a22ef0188cde1ce547682049c05903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88b342bb488023adc4d4",
+    "url": "/static/css/main.180f5dd2.chunk.css"
+  },
+  {
+    "revision": "aabd3aa392e8b8c26066",
+    "url": "/static/js/2.413d41a2.chunk.js"
+  },
+  {
+    "revision": "88b342bb488023adc4d4",
+    "url": "/static/js/main.c222d1aa.chunk.js"
+  },
+  {
+    "revision": "d0d095e5592b30651d3a",
+    "url": "/static/js/runtime~main.d2da7071.js"
+  }
+]);
