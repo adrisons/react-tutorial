4754284e9fb4025a2a1e0a828ab78865120026fb
Adrian Pereira Guerra <adries.91@gmail.com>
2019-08-21 11:23:50 +0200

    Updates

---
+++ precache-manifest.c73aa123eb8352d8cc45769f498955e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61df84cdb5d0801e349cede9b426f058",
+    "url": "/tic-tac-toe/index.html"
+  },
+  {
+    "revision": "88b342bb488023adc4d4",
+    "url": "/tic-tac-toe/static/css/main.180f5dd2.chunk.css"
+  },
+  {
+    "revision": "aabd3aa392e8b8c26066",
+    "url": "/tic-tac-toe/static/js/2.413d41a2.chunk.js"
+  },
+  {
+    "revision": "88b342bb488023adc4d4",
+    "url": "/tic-tac-toe/static/js/main.c222d1aa.chunk.js"
+  },
+  {
+    "revision": "2447e331fa6994ee754e",
+    "url": "/tic-tac-toe/static/js/runtime~main.96c826cc.js"
+  }
+]);
